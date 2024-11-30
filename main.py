f78d7ee8b3a9df92b78f989f2cad39661384491b
Dean <imadeureadis@gmail.com>
2024-11-30 14:16:06 +0100

    connect notifications

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import Flask
 from dotenv import load_dotenv
 from api.api import api
 from models.database import init_db
+from utils.scheduler import configure_scheduler
+from services.alerts import check_alerts
 
 
 load_dotenv()
@@ -18,6 +20,10 @@ if not API_KEY:
     raise EnvironmentError("API_KEY is not set in the environment variables.")
 
 init_db()
+
+# Configure the scheduler
+configure_scheduler(app, check_alerts)  # Pass your app and the function to the scheduler
+
 
 # Run the Flask web server on port 8000
 if __name__ == '__main__':
